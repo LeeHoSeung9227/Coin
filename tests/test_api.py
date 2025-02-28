b97379b2602abc683142a94f20f8b08995e386cc
LeeHoSeung9227 <dlghtmd0000@naver.com>
2025-02-28 17:41:43 +0900

    test upload

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,3 @@ import requests
 def test_home():
     response = requests.get("http://localhost")
     assert response.status_code == 200
-
-def test_api():
-    response = requests.get("http://localhost/api/")
-    assert response.status_code == 200
