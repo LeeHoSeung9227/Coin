74d31c750435c4b23702cf3d133eb2e100a75e72
LeeHoSeung9227 <dlghtmd0000@naver.com>
2025-05-29 18:48:24 +0900

    Update test_api.py
    임시 서버 설정 추가

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,8 @@
 import requests
+import responses
 
+@responses.activate
 def test_home():
+    responses.add(responses.GET, "http://localhost", json={}, status=200)
     response = requests.get("http://localhost")
     assert response.status_code == 200
